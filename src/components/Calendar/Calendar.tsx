a94a031f9181668c3b0504f3e7d57bd3030de2a1
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-26 13:25:37 -0400

    implement loading events

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,24 +1,116 @@
 import './Calendar.css';
+import { connect, ConnectedProps } from 'react-redux';
+import { RootState } from '../../redux/store';
+import {
+  selectUserEventsArray,
+  loadUserEvents,
+  UserEvent,
+} from '../../redux/user-events';
+import { useEffect } from 'react';
+import { addZero } from '../../lib/utils';
 
-const Calendar: React.FC = () => {
-  return (
+const mapState = (state: RootState) => ({
+  events: selectUserEventsArray(state),
+});
+
+const mapDispatch = {
+  loadUserEvents,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+interface Props extends PropsFromRedux {}
+
+const createDateKey = (date: Date) => {
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
+
+  return `${year}-${addZero(month)}-${addZero(day)}`;
+};
+
+const groupEventsByDay = (events: UserEvent[]) => {
+  const groups: Record<string, UserEvent[]> = {};
+
+  const addToGroup = (dateKey: string, event: UserEvent) => {
+    if (groups[dateKey] === undefined) {
+      groups[dateKey] = [];
+    }
+
+    groups[dateKey].push(event);
+  };
+
+  events.forEach((event) => {
+    const dateStartKey = createDateKey(new Date(event.dateStart));
+    const dateEndKey = createDateKey(new Date(event.dateEnd));
+
+    addToGroup(dateStartKey, event);
+
+    if (dateEndKey !== dateStartKey) {
+      addToGroup(dateEndKey, event);
+    }
+  });
+  return groups;
+};
+
+const Calendar: React.FC<Props> = ({ events, loadUserEvents }) => {
+  useEffect(() => {
+    loadUserEvents();
+  }, []);
+
+  let groupedEvents: ReturnType<typeof groupEventsByDay> | undefined;
+  let sortedGroupKeys: string[] | undefined;
+
+  if (events.length) {
+    groupedEvents = groupEventsByDay(events);
+    sortedGroupKeys = Object.keys(groupedEvents).sort(
+      (date1, date2) => +new Date(date1) - +new Date(date2)
+    );
+  }
+
+  return groupedEvents && sortedGroupKeys ? (
     <div className="calendar">
-      <div className="calendar-day">
-        <div className="calendar-day-label">
-          <span>1 February</span>
-        </div>
-        <div className="calendar-events">
-          <div className="calendar-event">
-            <div className="calendar-event-info">
-              <div className="calendar-event-time">10:00 - 12:00</div>
-              <div className="calendar-event-title">Learning TypeScript</div>
+      {sortedGroupKeys.map((dayKey) => {
+        const events = groupedEvents ? groupedEvents[dayKey] : [];
+        const groupDate = new Date(dayKey);
+        const day = groupDate.getDate();
+        const month = groupDate.toLocaleString(undefined, {
+          month: 'long',
+          timeZone: 'UTC',
+        });
+        return (
+          <div className="calendar-day">
+            <div className="calendar-day-label">
+              <span>
+                {day} {month}
+              </span>
             </div>
-            <button className="calendar-event-delete-button">&times;</button>
+            <div className="calendar-events">
+              {events.map((event) => {
+                return (
+                  <div key={event.id} className="calendar-event">
+                    <div className="calendar-event-info">
+                      <div className="calendar-event-time">10:00 - 12:00</div>
+                      <div className="calendar-event-title">
+                        {event.title}
+                      </div>
+                    </div>
+                    <button className="calendar-event-delete-button">
+                      &times;
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
+  ) : (
+    <p>Events are loading...</p>
   );
 };
 
-export default Calendar;
+export default connector(Calendar);
