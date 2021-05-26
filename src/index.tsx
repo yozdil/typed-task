5547d804d952b5600f7249ce28108d1a72f123c0
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-26 10:55:04 -0400

    clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App/App';
-import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
