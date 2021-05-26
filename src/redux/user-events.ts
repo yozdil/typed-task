c5b6958bd88adec8890d33b28d44f06b424dd903
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-26 09:58:43 -0400

    setup redux

---
+++ src/redux/user-events.ts
@@ -0,0 +1,34 @@
+import { AnyAction } from 'redux';
+
+interface UserEvent {
+  id: number;
+  title: string;
+  dateStart: string;
+  dateEnd: string;
+}
+
+interface UserEventsState {
+  byIds: Record<UserEvent['id'], UserEvent>;
+  allIds: UserEvent['id'][];
+}
+
+const initialState: UserEventsState = {
+  byIds: {},
+  allIds: [],
+};
+
+const userEventsReducer = (
+  state: UserEventsState = initialState,
+  action: AnyAction
+) => {
+  switch (action.type) {
+    // case value:
+
+    //   break;
+
+    default:
+      return state;
+  }
+};
+
+export default userEventsReducer;
